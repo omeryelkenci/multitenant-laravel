1cfeef7d884dd685e930b7721c568b293107a364
omer-y <om.yelkenci@gamil.com>
2021-10-02 16:11:37 +0300

    - Creating a tenant model

--- config/tenancy.php
+++ config/tenancy.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
+use App\Models\Tenant;
 use Stancl\Tenancy\Database\Models\Domain;
-use Stancl\Tenancy\Database\Models\Tenant;
 
 return [
     'tenant_model' => Tenant::class,
@@ -17,7 +17,6 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        '127.0.0.1',
         'localhost',
     ],
 
